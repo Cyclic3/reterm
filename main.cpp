dcfdd13f97619274ed724693a34f175e6c2e25e1
Cyclic3 <cyclic3.git@gmail.com>
2022-12-26 17:13:37 +0000

    Widgets work better on my machine

--- main.cpp
+++ main.cpp
@@ -1,36 +1,11 @@
-#include <QGuiApplication>
-#include <QQmlApplicationEngine>
+#include "mainwindow.hpp"
 
-#include <QLocale>
-#include <QTranslator>
+#include <QApplication>
 
 int main(int argc, char *argv[])
 {
-  qputenv("QT_IM_MODULE", QByteArray("qtvirtualkeyboard"));
-
-#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-  QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-#endif
-  QGuiApplication app(argc, argv);
-
-  QTranslator translator;
-  const QStringList uiLanguages = QLocale::system().uiLanguages();
-  for (const QString &locale : uiLanguages) {
-    const QString baseName = "reterm_" + QLocale(locale).name();
-    if (translator.load(":/i18n/" + baseName)) {
-      app.installTranslator(&translator);
-      break;
-    }
-  }
-
-  QQmlApplicationEngine engine;
-  const QUrl url(QStringLiteral("qrc:/main.qml"));
-  QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-                   &app, [url](QObject *obj, const QUrl &objUrl) {
-    if (!obj && url == objUrl)
-      QCoreApplication::exit(-1);
-  }, Qt::QueuedConnection);
-  engine.load(url);
-
-  return app.exec();
+  QApplication a(argc, argv);
+  MainWindow w;
+  w.show();
+  return a.exec();
 }
